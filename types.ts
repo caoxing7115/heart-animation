af80ebd32b28f24da06e4a2f170b73c1370a5a8c
caoxing7115 <cxing7115@gmail.com>
2025-12-31 14:08:51 +0800

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,24 @@
+
+export interface TimeLeft {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+export interface NewYearWish {
+  chinese: string;
+  english: string;
+  author: string;
+}
+
+export interface FireworkParticle {
+  x: number;
+  y: number;
+  vx: number;
+  vy: number;
+  color: string;
+  alpha: number;
+  decay: number;
+  size: number;
+}
